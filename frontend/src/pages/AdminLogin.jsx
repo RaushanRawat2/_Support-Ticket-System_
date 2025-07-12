53c6a08bcfa74dd7fe35b9096ee16defd45ecd78
Raushan Kumar <166208082+RaushanRawat2@users.noreply.github.com>
2025-07-12 19:05:10 +0530

    Update AdminLogin.jsx

--- frontend/src/pages/AdminLogin.jsx
+++ frontend/src/pages/AdminLogin.jsx
@@ -20,7 +20,7 @@ const AdminLogin = () => {
   const handleSubmit = async e => {
   e.preventDefault();
   try {
-    const res = await axios.post('http://localhost:5000/api/adminLogin', formData);
+    const res = await axios.post('https://support-ticket-system-backend-6g7z.onrender.com/api/adminLogin', formData);
 
     if (res.data.success) {
       setError('');
